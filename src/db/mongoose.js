72bad561a684832b4fe8e6102900548e7fbea3ee
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-02 23:52:47 +0100

    Data validation and Sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,73 +6,73 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   //   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Email is invalid");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    validate(value) {
-      if (!validator.isStrongPassword(value)) {
-        throw new Error("Your password is not secure");
-      }
-    },
-  },
-  age: {
-    type: Number,
-    validate(value) {
-      if (value < 0) {
-        throw new Error("Age must be a positive number");
-      }
-    },
-  },
-});
-
-const me = new User({
-  name: "  Touluwanimi   ",
-  age: "27",
-  email: "Tolu@gmail.com",
-  password: "Wordpassni-01",
-});
-
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((err) => {
-    console.log("Error!", err);
-  });
-
-// const Task = mongoose.model("Tasks", {
-//   description: {
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     trim: true,
+//   },
+//   email: {
 //     type: String,
 //     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Email is invalid");
+//       }
+//     },
 //   },
-//   completed: {
-//     type: Boolean,
+//   password: {
+//     type: String,
 //     required: true,
+//     validate(value) {
+//       if (!validator.isStrongPassword(value)) {
+//         throw new Error("Your password is not secure");
+//       }
+//     },
+//   },
+//   age: {
+//     type: Number,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error("Age must be a positive number");
+//       }
+//     },
 //   },
 // });
 
-// const me = new Task({
-//   description: "House Cleaning",
-//   completed: true,
+// const me = new User({
+//   name: "  Touluwanimi   ",
+//   age: "27",
+//   email: "Tolu@gmail.com",
+//   password: "Wordpassni-01",
 // });
+
 // me.save()
 //   .then(() => {
 //     console.log(me);
 //   })
-//   .catch((err) => console.log("Error!", err));
+//   .catch((err) => {
+//     console.log("Error!", err);
+//   });
+
+const Task = mongoose.model("Tasks", {
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  completed: {
+    type: Boolean,
+  },
+});
+
+const me = new Task({
+  description: "House Cleaning",
+  completed: true,
+});
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((err) => console.log("Error!", err));
