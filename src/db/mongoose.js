1fc40aeabe430ae6c4147de7888b287d824e010e
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-06 20:11:55 +0100

    setting up a mongoose model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,55 +6,55 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   //   useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//     trim: true,
-//   },
-//   email: {
-//     type: String,
-//     required: true,
-//     trim: true,
-//     lowercase: true,
-//     validate(value) {
-//       if (!validator.isEmail(value)) {
-//         throw new Error("Email is invalid");
-//       }
-//     },
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     validate(value) {
-//       if (!validator.isStrongPassword(value)) {
-//         throw new Error("Your password is not secure");
-//       }
-//     },
-//   },
-//   age: {
-//     type: Number,
-//     validate(value) {
-//       if (value < 0) {
-//         throw new Error("Age must be a positive number");
-//       }
-//     },
-//   },
-// });
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isStrongPassword(value)) {
+        throw new Error("Your password is not secure");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
+  },
+});
 
-// const me = new User({
-//   name: "  Touluwanimi   ",
-//   age: "27",
-//   email: "Tolu@gmail.com",
-//   password: "Wordpassni-01",
-// });
+const me = new User({
+  name: "  Touluwanimi   ",
+  age: "27",
+  email: "Tolu@gmail.com",
+  password: "Wordpassni-01",
+});
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((err) => {
-//     console.log("Error!", err);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((err) => {
+    console.log("Error!", err);
+  });
 
 const Task = mongoose.model("Tasks", {
   description: {
