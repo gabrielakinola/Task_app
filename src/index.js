d0aac354812a4183f86a83cc05e1bad7f770af59
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-15 11:05:21 +0100

    promise chaining

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
 const port = process.env.PORT || 3000;
-
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
-  useNewUrlParser: true,
-});
 
 app.use(express.json());
 
