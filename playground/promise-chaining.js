3c02b54434c0ddb9ddde8848dcbd9794bb90c8b2
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-15 12:26:38 +0100

    promise chaining

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,1 +1,14 @@
 require("../src/db/mongoose");
+
+const User = require("../src/models/user");
+
+User.findByIdAndUpdate("6320569e7e65decdc93aad27", { age: 27 })
+  .then((user) => {
+    console.log(user);
+    return User.countDocuments({ age: 27 }).then((result) => {
+      console.log(result);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
