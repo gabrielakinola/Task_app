a72fbc21ee569f75bec828adf5cb6d9b9bcd6fe1
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-09-19 23:04:35 +0100

    Using router to seperate route files

---
+++ src/routers/task.js
@@ -0,0 +1,102 @@
+const express = require("express");
+const Task = require("../models/task");
+const router = express.Router();
+
+router.post("/tasks", async (req, res) => {
+  const user = new Task(req.body);
+
+  try {
+    await user.save();
+    res.send(user).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+//   user
+//     .save()
+//     .then((task) => {
+//       res.send(task);
+//     })
+//     .catch((e) => {
+//       res.status(400).send(e);
+//     });
+// });
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+//   Task.find({})
+//     .then((tasks) => {
+//       res.send(tasks);
+//     })
+//     .catch((e) => {
+//       res.status(500).send(e);
+//     });
+// });
+
+router.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.send("There is no task with that id").status(500);
+    }
+    res.send(task);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+  // Task.findById(_id)
+  //   .then((task) => {
+  //     if (!task) {
+  //       return res.status(404).send();
+  //     }
+  //     res.send(task);
+  //   })
+  //   .catch((e) => {
+  //     res.status(500).send(e);
+  //   });
+});
+
+router.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["task", "completed"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: " Invalid Upadates!" });
+  }
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!task) {
+      return res.status(400).send("No tasks matches that id");
+    }
+    res.send(task);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  const task = await Task.findByIdAndDelete(req.params.id);
+  try {
+    if (!task) {
+      return res.status(404).send(`Task not found`);
+    }
+    res.send(task);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+module.exports = router;
---
+++ src/routers/user.js
@@ -0,0 +1,112 @@
+const express = require("express");
+const User = require("../models/user");
+
+const router = express.Router();
+
+//ROute to create a new user
+router.post("/users", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+//Without async await
+//   user
+//     .save()
+//     .then((user) => {
+//       res.send(user);
+//     })
+//     .catch((e) => {
+//       res.status(400).send(e);
+//     });
+// });
+
+//Route to get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+//Without async await
+//   User.find({})
+//     .then((users) => {
+//       res.send(users);
+//     })
+//     .catch((e) => {
+//       res.status(500).send(e);
+//     });
+// });
+
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(400).send("There is no user with that id");
+    }
+    res.send(user).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+// without async await
+//   User.findById(_id)
+//     .then((user) => {
+//       if (!user) {
+//         return res.status(404).send();
+//       }
+//       res.send(user);
+//     })
+//     .catch((e) => {
+//       res.status(500).send(e);
+//     });
+// });
+
+//Resource Creating Endpoints
+router.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: " Invalid Upadates!" });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+//Resource deleting endpoints
+router.delete("/users/:id", async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+  try {
+    if (!user) {
+      return res.status(404).send(`User not found!`);
+    }
+    res.send(user);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+module.exports = router;
