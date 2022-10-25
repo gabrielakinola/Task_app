c877d73abf162cde239a3749f4391a46b31730f9
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-10-25 19:58:28 +0100

    Setting up environment variables

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    const decoded = jwt.verify(token, "thisismynewcourse");
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
     // console.log(decoded);
     // console.log(jwt);
     const user = await User.findOne({
