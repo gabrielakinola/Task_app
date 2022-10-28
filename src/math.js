d7bf107c0002c04a9891c95c23d82e6533367395
gabrielakinola <gabrieltemitopeakinola@gmail.com>
2022-10-28 13:32:29 +0100

    Writing Jest Tests and assertions

--- src/math.js
+++ src/math.js
@@ -1,7 +1,5 @@
-const calculateTip = (total, tipPercent) => {
-  const tip = total * tipPercent;
-  return total + tip;
-};
+const calculateTip = (total, tipPercent) => total + total * tipPercent;
+
 module.exports = {
   calculateTip,
 };
